e003d07011d0601c929cf46522c177b75a362a1b
Antonia <antonia@Macbook-A-4.local>
2017-04-13 13:09:59 +0100

    Initial commit

---
+++ menumachine/menubar/menuspecs.js
@@ -0,0 +1,52 @@
+/*
+MenuMachine 2 definition file - do not edit. http://menumachine.com
+2.1.1 :: menubar
+*/
+var menuName="menubar";
+var pkg=new menuPackage(menuName,0,0,0,0,0,0,0,0,1);
+/*s*/
+pkg.aB("block1p","",48.5,1.5625,0,0,0,0,0,0,0,0,0,"h",100,"",0.0625,0,"",0,"black",0,0,"black",0,0,0,"",/*URL*/"images/arrmodsmldown_8b8b8b.gif",/*URL*/"images/arrmodsmldown_990000.gif",/*URL*/"images/arrmodsmldown_white.gif",0.25,0.4375,0.625,0.625,0,0,0,0,0);
+pkg.aI("menu6p","block1p",6.875,1.5625,0,0,"Writings","",/*URL*/"../../writings/tgworks.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"Contains all published writings and public speeches","",0,0,"Verdana,Arial,Helvetica,sans-serif","#bababa","#333333",0.6875,1,0,0.0625,"#990000",0,1,0,0,0.25,"",1,"","#aaaaaa","#990000",0.6875,1,0,0.0625,"#990000",0,1,0,0,0.25,"",0);
+pkg.aI("m1r4iylbp","block1p",6.875,1.5625,6.9375,0,"Policy","",/*URL*/"../../policy/irlinks.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"Links to UK and internional sources","",1,1);
+pkg.aI("m2mdsiju","block1p",6.875,1.5625,13.875,0,"Apple","",/*URL*/"../../apple/tgapple.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"Computer links","",2,1);
+pkg.aI("m2mdsijv","block1p",6.875,1.5625,20.8125,0,"Links","",/*URL*/"../../links/tgcool.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"General reference links","",3,1);
+pkg.aI("m2mdsijt","block1p",6.875,1.5625,27.75,0,"Weblog","",/*URL*/"http://homepage.mac.com/tgarden/iblog/index.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"Blog is on Text - family photos for current year and earlier","",4,1);
+pkg.aI("m2mdsijw","block1p",6.875,1.5625,34.6875,0,"Biography","",/*URL*/"../../biog/tgbio.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"Long bio has hot links","",5,1);
+pkg.aI("m1r4iylcp","block1p",6.875,1.5625,41.625,0,"News","",/*URL*/"../../news/news.html","right",/*URL*/"images/burgundy_pro.gif",/*URL*/"images/burgundy_pro_click.gif",/*URL*/"images/burgundy_pro_click.gif",0.375,0.375,0,0,"Key international news links","",6,1);
+pkg.aB("m1zcqoggp","menu6p",47.8125,1.5,2.1875,1.5625,35,25,0,0,0,0,0,"h",100,"",0.0625,0.0625,"#a60713",0.0625,"#aaaaaa",0,1,"#AAAAAA",0.0625,0,0,"",/*URL*/"images/arrmodsmlright_8b8b8b.gif",/*URL*/"images/arrmodsmlright_white.gif",/*URL*/"images/arrmodsmlright_white.gif",0.125,0.375,0.625,0.625,0,0,0,0,0);
+pkg.aI("m1zcqoghp","m1zcqoggp",3.75,1.25,0.125,0.125,"2006/07","",/*URL*/"../../writings/tgworks.html","right","","","",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#eeeeee","black",0.6875,0,0,0,"white",0,1,0,0,0.1875,"",1,_,"#990000","white",_,0,_,0,"white",_,_,_,_,0.1875,_,0);
+pkg.aI("m31r0tdi","m1zcqoggp",3.75,1.25,4.0625,0.125,"2005","",/*URL*/"../../writings/tgworks05.html","right","","","",0,0,0,0,"","",1,1);
+pkg.aI("m1r4iyl6p","m1zcqoggp",3.75,1.25,8,0.125,"2004","",/*URL*/"../../writings/tgworks2004.html","right","","","",0,0,0,0,"","",2,1);
+pkg.aI("m1r4iyl7p","m1zcqoggp",3.75,1.25,11.9375,0.125,"2003","",/*URL*/"../../writings/tgworks2003.html","right","","","",0,0,0,0,"","",3,1);
+pkg.aI("m2mdsiji","m1zcqoggp",3.75,1.25,15.875,0.125,"2002","",/*URL*/"../../writings/tgworks2002.html","right","","","",0,0,0,0,"","",4,1);
+pkg.aI("m2mdsijj","m1zcqoggp",3.75,1.25,19.8125,0.125,"2001","",/*URL*/"../../writings/tgworks2001.html","right","","","",0,0,0,0,"","",5,1);
+pkg.aI("m2mdsijk","m1zcqoggp",3.75,1.25,23.75,0.125,"2000","",/*URL*/"../../writings/tgworks2000.html","right","","","",0,0,0,0,"","",6,1);
+pkg.aI("m2mdsijl","m1zcqoggp",3.75,1.25,27.6875,0.125,"1999","",/*URL*/"../../writings/tgworks1999.html","right","","","",0,0,0,0,"","",7,1);
+pkg.aI("m2mdsijn","m1zcqoggp",3.75,1.25,31.625,0.125,"Books","",/*URL*/"../../writings/books.html","right","","","",0,0,0,0,"","",8,1);
+pkg.aI("m2mdsijo","m1zcqoggp",3.75,1.25,35.5625,0.125,"Lectures","",/*URL*/"../../writings/Lectures.html","right","","","",0,0,0,0,"","",9,1);
+pkg.aI("m2mdsijp","m1zcqoggp",3.75,1.25,39.5,0.125,"Reviews","",/*URL*/"../../writings/reviews.html","right","","","",0,0,0,0,"","",10,1);
+pkg.aI("m2mdsijq","m1zcqoggp",4.25,1.25,43.4375,0.125,"All by title","",/*URL*/"../../writings/allworks.html","right","","","",0,0,0,0,"","",11,1);
+pkg.aB("m2qguejw","m1r4iylbp",13.375,1.5,9.125,1.5625,35,25,0,0,0,0,0,"h",100,"",0.0625,0.0625,"#a60713",0.0625,"#aaaaaa",0,1,"#AAAAAA",0.0625,0,0,"",/*URL*/"images/arrmodsmlright_8b8b8b.gif",/*URL*/"images/arrmodsmlright_white.gif",/*URL*/"images/arrmodsmlright_white.gif",0.125,0.375,0.625,0.625,0,0,0,0,0);
+pkg.aI("m2qguejx","m2qguejw",4.25,1.25,0.125,0.125,"Official","",/*URL*/"../../policy/irlinks.html","right","","","",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#eeeeee","black",0.6875,0,0,0,"white",0,1,0,0,0.1875,"",1,_,"#990000","white",_,0,_,0,"white",_,_,_,_,0.1875,_,0);
+pkg.aI("m2qguejy","m2qguejw",4.25,1.25,4.5625,0.125,"NGOs","",/*URL*/"../../policy/ngo.html","right","","","",0,0,0,0,"","",1,1);
+pkg.aI("m2qguejz","m2qguejw",4.25,1.25,9,0.125,"Europe","",/*URL*/"../../policy/eu.html","right","","","",0,0,0,0,"","",2,1);
+pkg.aB("m2qguek3","m2mdsiju",13.375,1.5,16.0625,1.5625,35,25,0,0,0,0,0,"h",100,"",0.0625,0.0625,"#a60713",0.0625,"#aaaaaa",0,1,"#AAAAAA",0.0625,0,0,"",/*URL*/"images/arrmodsmlright_8b8b8b.gif",/*URL*/"images/arrmodsmlright_white.gif",/*URL*/"images/arrmodsmlright_white.gif",0.125,0.375,0.625,0.625,0,0,0,0,0);
+pkg.aI("m2qguek4","m2qguek3",4.25,1.25,0.125,0.125,"Software","",/*URL*/"../../apple/tgapple.html","right","","","",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#eeeeee","black",0.6875,0,0,0,"white",0,1,0,0,0.1875,"",1,_,"#990000","white",_,0,_,0,"white",_,_,_,_,0.1875,_,0);
+pkg.aI("m2qguek5","m2qguek3",4.25,1.25,4.5625,0.125,"Palm","",/*URL*/"../../apple/tgapple3.html","right","","","",0,0,0,0,"","",1,1);
+pkg.aI("m2qguek6","m2qguek3",4.25,1.25,9,0.125,"Hardware","",/*URL*/"../../apple/tgapple4.html","right","","","",0,0,0,0,"","",2,1);
+pkg.aB("m2qhzdnf","m2mdsijv",17.8125,1.5,23,1.5625,35,25,0,0,0,0,0,"h",100,"",0.0625,0.0625,"#a60713",0.0625,"#aaaaaa",0,1,"#AAAAAA",0.0625,0,0,"",/*URL*/"images/arrmodsmlright_8b8b8b.gif",/*URL*/"images/arrmodsmlright_white.gif",/*URL*/"images/arrmodsmlright_white.gif",0.125,0.375,0.625,0.625,0,0,0,0,0);
+pkg.aI("m2qhzdng","m2qhzdnf",4.25,1.25,0.125,0.125,"Quick","",/*URL*/"../../text.html","right","","","",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#eeeeee","black",0.6875,0,0,0,"white",0,1,0,0,0.1875,"",1,_,"#990000","white",_,0,_,0,"white",_,_,_,_,0.1875,_,0);
+pkg.aI("m2qhzdnh","m2qhzdnf",4.25,1.25,4.5625,0.125,"Internet","",/*URL*/"../../links/tgcool.html","right","","","",0,0,0,0,"","",1,1);
+pkg.aI("m2qhzdni","m2qhzdnf",4.25,1.25,9,0.125,"Reference","",/*URL*/"../../links/tgcool1.html","right","","","",0,0,0,0,"","",2,1);
+pkg.aI("m2qhzdnj","m2qhzdnf",4.25,1.25,13.4375,0.125,"Misc","",/*URL*/"../../links/tgcool2.html","right","","","",0,0,0,0,"","",3,1);
+pkg.aB("m2qguek0","m2mdsijt",11.75,1.5,29.9375,1.5625,35,25,0,0,0,0,0,"h",100,"",0.0625,0.0625,"#a60713",0.0625,"#aaaaaa",0,1,"#AAAAAA",0.0625,0,0,"",/*URL*/"images/arrmodsmlright_8b8b8b.gif",/*URL*/"images/arrmodsmlright_white.gif",/*URL*/"images/arrmodsmlright_white.gif",0.125,0.375,0.625,0.625,0,0,0,0,0);
+pkg.aI("m2qguek1","m2qguek0",3.75,1.25,0.125,0.125,"Text","",/*URL*/"http://homepage.mac.com/tgarden/iblog/index.html","right","","","",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#eeeeee","black",0.6875,0,0,0,"white",0,1,0,0,0.1875,"",1,_,"#990000","white",_,0,_,0,"white",_,_,_,_,0.1875,_,0);
+pkg.aI("m2qguek2","m2qguek0",3.6875,1.25,4.0625,0.125,"Photo","",/*URL*/"http://web.mac.com/tgarden/iWeb/Tim Garden Site/0603.html","right","","","",0,0,0,0,"","",1,1);
+pkg.aI("m19ttqp1","m2qguek0",3.6875,1.25,7.9375,0.125,"pre 06","",/*URL*/"http://homepage.mac.com/tgarden/PhotoAlbum30.html","right","","","",0,0,0,0,"","",2,1);
+pkg.aB("m2mdsijx","m2mdsijw",13.9375,1.5,34.625,1.5625,-1,25,0,0,0,0,0,"h",100,"",0.0625,0.0625,"#a60713",0.0625,"#aaaaaa",0,0,"black",0,0,0,"",/*URL*/"images/arrmodsmlright_8b8b8b.gif",/*URL*/"images/arrmodsmlright_white.gif",/*URL*/"images/arrmodsmlright_white.gif",0.125,0.375,0.625,0.625,0,0,0,0,0);
+pkg.aI("m2mdsijy","m2mdsijx",6.75,1.25,0.125,0.125,"Short Bio","",/*URL*/"../../biog/tgbio.html","right","","","",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#eeeeee","black",0.6875,0,0,0,"white",0,1,0,0,0.1875,"",1,_,"#990000","white",_,0,_,0,"white",_,_,_,_,0.1875,_,0);
+pkg.aI("m2mdsijz","m2mdsijx",6.875,1.25,6.9375,0.125,"Long Bio","",/*URL*/"../../biog/tgbio1.html","right","","","",0,0,0,0,"","",1,1);
+/*f*/
+__menuHolder.aP(pkg);
+var __l=__br.un?"un.js":(__br.ie?"iedom.js":(__br.dom?"w3cdom.js":""));
+document.write(_sTs+mmfolder+"core/"+__l+_sTe);
